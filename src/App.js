bab285c080d7fd2abcfa13a7ae671b6156cfa4b8
nsns0101 <nsns0101@naver.com>
2020-05-13 07:31:16 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    constructor(props) {
+        super(props);
+        this.state = {
+            username:null
+        };
+    }
+
+  componentDidMount() {
+      axios('http://localhost:3001/api')
+          .then( res => {
+            // console.log(res.data.username);
+            this.setState({username : res.data.username})
+          })
+  }
+
+  render() {
+    const {username} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            {username ? `Hello ${username}` : 'Hello World'}
+          </header>
+        </div>
+    );
+    ;
+  }
 }
 
 export default App;
